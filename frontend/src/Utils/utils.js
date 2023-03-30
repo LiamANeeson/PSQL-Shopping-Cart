dc97ebc78159fffb3f868777ac64356413ff308d
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2023-03-30 17:34:42 +0100

    Added getItemId conenction to Frontend from Backend

--- frontend/src/Utils/utils.js
+++ frontend/src/Utils/utils.js
@@ -8,4 +8,13 @@ export const getAllItems = () => {
         console.error(error.message);
         throw new Error("Error Retrieving Data");
       });
-  }
+}
+
+export const getItemId = (item_id) => {
+    return axios.get(`http://localhost:8000/items/${item_id}`)
+      .then(response => response.data)
+      .catch(error => {
+        console.error(error.message);
+        throw new Error("Error retrieving Data")
+    });
+}
